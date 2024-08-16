78af31fdfe07efba9b2feffd4e7ba277b7018ef1
Soumojit Poddar <poddarsoumojit7@gmail.com>
2024-08-16 21:54:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.innerHTML == '='){
+            try {
+                string = eval(string);
+            } catch {
+                string = "Error";
+            }
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.classList.contains('fa-delete-left') || e.target.classList.contains('delIcon')){
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
